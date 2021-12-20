4ec5462b015b08d4b27ca10d2b3765a42c26bcb7
Shaun S <shaun.sticka@outlook.com>
2021-12-19 19:12:22 -0600

    pushed for help

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mysql = require("mysql2");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +7,19 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: "localhost",
+        // your MySQL username,
+        user: "miyadamanshaun@outlook.com",
+        // your MyQL password
+        password: "Welcomelb$8",
+        database: "election"
+    },
+    console.log("Connected to the election database.")
+);
 
 // Default repsonse for any other request (Not Found)
 app.use((req, res) => {
